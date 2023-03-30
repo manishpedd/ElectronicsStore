f6f7a8d74f9f9ce171630c89e1a911850610413a
man78 <manish.peddurwar@gmail.com>
2023-03-30 14:24:59 +0530

    changes done

--- src/main/java/com/electronics/store/entities/User.java
+++ src/main/java/com/electronics/store/entities/User.java
@@ -14,7 +14,7 @@ import javax.persistence.*;
 @Builder
 @Entity
 @Table(name = "users")
-public class User {
+public class User extends BaseEntity{
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
