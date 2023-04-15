585320cba210ca35aba3f29b1718730bd3d122cf
man78 <manish.peddurwar@gmail.com>
2023-04-15 17:20:14 +0530

    Added abstract class base test

--- src/test/java/com/electronics/store/ElectronicsStoreApplicationTests.java
+++ src/test/java/com/electronics/store/ElectronicsStoreApplicationTests.java
@@ -2,12 +2,14 @@ package com.electronics.store;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-//
-//@SpringBootTest
-//class ElectronicsStoreApplicationTests {
-//
-//	@Test
-//	void contextLoads() {
-//	}
-//
-//}
+
+@SpringBootTest
+class ElectronicsStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+
+		System.out.println("Testing our project");
+	}
+
+}
---
+++ src/test/java/com/electronics/store/services/BaseTest.java
@@ -0,0 +1,9 @@
+package com.electronics.store.services;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles
+@SpringBootTest
+public abstract class BaseTest {
+}
